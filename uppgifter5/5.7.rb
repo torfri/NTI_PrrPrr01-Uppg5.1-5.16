df8ab85572c3257f186f89b42434fbf9a01b5c8c
Torkel Frings <torkel.frings@learnet.se>
2020-10-18 15:27:22 +0200

    hypotenus whit test for 1 senario

--- uppgifter5/5.7.rb
+++ uppgifter5/5.7.rb
@@ -1,5 +1,20 @@
 def hypotenus(num1, num2)
-    return math.sqrt(num1**2 + num2**2)
+    return Math.sqrt(num1**2 + num2**2)
 end
 
+#tests for hypotenus
 
+def test_hypotenus(antal)
+    for i in 0..antal
+        x=3
+        y=x**2-4    #3**2-4 = 5
+        z=x**2-y    #3**2-5 = 4
+        if x != hypotenus(Math.sqrt(y), Math.sqrt(z))
+            return "error #{x}**2 != #{y}**2 + #{z}**2"
+        end
+        print "."
+    end
+    return "✔️"
+end
+
+puts test_hypotenus(100)
