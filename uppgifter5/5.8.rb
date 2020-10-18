32cfed597992aae4bb178cd9132b78f675bea24b
Torkel Frings <torkel.frings@learnet.se>
2020-10-18 16:02:09 +0200

    absolut whit tests

--- uppgifter5/5.8.rb
+++ uppgifter5/5.8.rb
@@ -5,3 +5,17 @@ def absolut(num)
     return num
 end
 
+#test fo absolut
+
+def test_absolut(antal)
+    for i in 0..antal
+        x= rand(-100.0..100.0)
+        if x.abs != absolut(x)
+            return "error #{x}"
+        end
+        print "."
+    end
+    return "✔️"
+end
+
+puts test_absolut(100)
