05af4900022ea8dcd42325d84814ec5b7d47ef01
Torkel Frings <torkel.frings@learnet.se>
2020-10-18 14:44:35 +0200

    first substrakt 2 inputs whit tests

--- uppgifter5/5.6.rb
+++ uppgifter5/5.6.rb
@@ -1,10 +1,23 @@
-def subtrakt(num1,num2)
-    return num1-num2
+
+def subtrakt_from_bigest(num1, num2)
+    if num1 > num2
+        return num1 -num2
+    end
+    return num2 - num1
 end
 
-def subtrakt_from_bigets(num1, num2)
-    if num1 > num2
-        return subtrakt(num1, num2)
+#test for subtrakt_from_bigest
+def test_subtrakt_from_bigest(antal)
+    for i in 0..antal
+        x= rand(100)
+        y= rand(100)
+        z= x+y
+        if x != subtrakt_from_bigest(z,y)
+            return "error x=#{x} z=#{z} y=#{y}"
+        end
+        print "."
     end
-    return subtrakt(num2, num1)
+    return "✔️"
 end
+
+puts test_subtrakt_from_bigest(100)
