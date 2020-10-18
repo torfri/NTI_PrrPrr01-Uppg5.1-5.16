31c90c58864315a08cac13b2f05a3ec5b2d0db6d
Torkel Frings <torkel.frings@learnet.se>
2020-10-18 16:13:46 +0200

    is_negative whit tests

--- uppgifter5/5.9.rb
+++ uppgifter5/5.9.rb
@@ -1,6 +1,21 @@
 def is_negativ(num)
     if num >= 0
-        return true
+        return false       
     end
-    return false
+    return true     # if negativ the output is now true
 end
+
+#tests for is_negativ
+
+def test_is_negativ(antal)
+    for i in 0..antal
+        x = rand(-100..100)
+        if x.negative? != is_negativ(x)
+            return "error #{x} #{is_negativ(x)}"
+        end
+        print "."
+    end
+    return "✔️"
+end
+
+puts test_is_negativ(100)
