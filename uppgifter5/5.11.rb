12c48e897b9639f1156aa85eaecb96c7da3e7d74
Torkel Frings <torkel.frings@learnet.se>
2020-10-18 16:26:04 +0200

    is_even whit tests

--- uppgifter5/5.11.rb
+++ uppgifter5/5.11.rb
@@ -4,3 +4,18 @@ def is_even (num)
     end
     return false
 end
+
+# tests for is even
+
+def test_is_even(antal)
+    for i in 0..antal 
+        x= rand(100)
+        if x.even? != is_even(x)
+            return "error #{x}"
+        end
+        print "."
+    end
+    return "✔️"
+end
+
+puts test_is_even(100)
